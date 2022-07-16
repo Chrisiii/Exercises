b60791dfdb5c6124351513f71ca0930538aad6c5
unknown <prikhodachristina@gmail.com>
2022-07-16 19:40:49 +0400

    performed 11 exercises

--- Exercise2/app.js
+++ Exercise2/app.js
@@ -11,13 +11,21 @@ function stringItUp(arr){
 }
 
 
-//3. Make an array of strings of the names
+// 3) Capitalize each of an array of names
+
+function capitalizeNames(arr){
+    return arr.map(element => {
+        return element.charAt(0).toUpperCase() + element.substring(1).toLowerCase()
+    })
+}
+
+//4. Make an array of strings of the names
 
 function namesOnly(arr){
     return arr.map(item => item.name)
 }
 
-//4.Make an array of strings of the names saying whether or not they can go to The Matrix
+//5.Make an array of strings of the names saying whether or not they can go to The Matrix
 
 function makeStrings(arr){
     return arr.map(el => {
@@ -32,7 +40,7 @@ function makeStrings(arr){
     })
 }
 
-//5. If the given input is an array of numbers, return the sum of all the positives ones. If the array is empty or there aren't any positive numbers, return 0
+//6. If the given input is an array of numbers, return the sum of all the positives ones. If the array is empty or there aren't any positive numbers, return 0
 
 
 function sum(arr){
@@ -49,7 +57,7 @@ function sum(arr){
     return sum
 }
 
-//6.Find the difference in age between the oldest and youngest family members, and return their respective ages and the age difference.
+//7.Find the difference in age between the oldest and youngest family members, and return their respective ages and the age difference.
 
 function differenceInAge(arr){
 
@@ -67,7 +75,7 @@ function differenceInAge(arr){
     
 }
 
-//7.Count the occurrences of distinct elements in the given 2D array. The given input is an array,
+//8.Count the occurrences of distinct elements in the given 2D array. The given input is an array,
 // the elements of which are arrays of strings. The result is an object whose
 // property names are the values from the arrays and their value is the number of their occurrences.
 
@@ -84,7 +92,7 @@ function foo(arr){
 }
 
 
-//8.Given an array nums of size n, return the majority element.
+//9.Given an array nums of size n, return the majority element.
 // The majority element is the element that appears more than ⌊n / 2⌋ times.
 // You may assume that the majority element always exists in the array.
 
@@ -102,7 +110,7 @@ function majorityElement(arr){
 }
 
 
-//9gtnel trvats array i miji 3rd amenashate krknvox tive
+//10gtnel trvats array i miji 3rd amenashate krknvox tive
 
 function thirdElem(arr){
 
@@ -121,7 +129,7 @@ function thirdElem(arr){
 
 
 
-//10.Given a string s and an array of strings words, determine whether s is a prefix string of words.
+//11.Given a string s and an array of strings words, determine whether s is a prefix string of words.
 // A string s is a prefix string of words if s can be made by concatenating the first k strings in words for some positive k no larger than words.length.
 // Return true if s is a prefix string of words, or false otherwise.
 
